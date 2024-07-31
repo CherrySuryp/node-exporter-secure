7526e7cdf5a0e5f5699346a7884b5e3f2a4f1cca
CherrySuryp <hackyourrself@gmail.com>
2024-07-31 13:11:32 +0200

    ✨Added install.sh script

--- installation/install.sh
+++ installation/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+wget -O docker-compose.yml https://raw.githubusercontent.com/CherrySuryp/node-exporter-secure/master/installation/docker-compose.yml
+
 read -p "Enter the new API_BEARER_TOKEN: " new_token
 sed -i '' "s/API_BEARER_TOKEN: \".*\"/API_BEARER_TOKEN: \"$new_token\"/" docker-compose.yml
 echo "API_BEARER_TOKEN has been updated."
+
+docker compose up -d
