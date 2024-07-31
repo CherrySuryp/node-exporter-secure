01cabb99020fb270d828bf0f9cceb202647c922e
CherrySuryp <hackyourrself@gmail.com>
2024-07-31 13:27:54 +0200

    ✨Added install.sh script

---
+++ install/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O docker-compose.yml https://raw.githubusercontent.com/CherrySuryp/node-exporter-secure/master/install/docker-compose.yml
--- installation/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -O docker-compose.yml https://raw.githubusercontent.com/CherrySuryp/node-exporter-secure/master/installation/docker-compose.yml
